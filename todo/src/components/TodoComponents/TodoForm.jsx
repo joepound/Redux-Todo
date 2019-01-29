1ee215589f62d75a55cdc4cc2c3d7fa76e0b2f75
joepound <14357797+joepound@users.noreply.github.com>
2019-01-29 15:00:59 -0700

    Added focus effect & animation on form for adding new todos

--- todo/src/components/TodoComponents/TodoForm.jsx
+++ todo/src/components/TodoComponents/TodoForm.jsx
@@ -2,12 +2,10 @@ import React from "react";
 
 const TodoForm = props => {
   return (
-    <form className="todo-app__add-form">
-      <input
-        className="todo-app__add-form__task-input"
-        placeholder="Enter a task"
-      />
-    </form>
+    <input
+      className="todo-app__add-todo-input"
+      placeholder="Enter a task"
+    />
   );
 };
 
