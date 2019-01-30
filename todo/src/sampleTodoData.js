e5e14d273de99068bd254e73dd0bd2e07ae8ff96
joepound <14357797+joepound@users.noreply.github.com>
2019-01-30 07:02:42 -0700

    Added separate file to hold default sample todo items

---
+++ todo/src/sampleTodoData.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    id: "439856382475698734265",
+    task: "Learn React hooks",
+    completed: false
+  },
+  {
+    id: "247569439856388734265",
+    task: "Learn Redux",
+    completed: true
+  },
+  {
+    id: "569873424398563824765",
+    task: "Learn TypeScript",
+    completed: false
+  }
+];
--- todo/src/store/reducers/todosReducer.js
+++ todo/src/store/reducers/todosReducer.js
@@ -6,24 +6,10 @@ import {
   CLEAR_COMPLETED_TODOS
 } from "../actions/types";
 
+import sampleTodoData from "../../sampleTodoData";
+
 const initialState = {
-  todos: [
-    {
-      id: "439856382475698734265",
-      task: "Learn React hooks",
-      completed: false
-    },
-    {
-      id: "247569439856388734265",
-      task: "Learn Redux",
-      completed: true
-    },
-    {
-      id: "569873424398563824765",
-      task: "Learn TypeScript",
-      completed: false
-    }
-  ],
+  todos: JSON.parse(localStorage.getItem("todos")) || sampleTodoData,
   newTaskInput: ""
 };
 
